cf282bffa898d86963f6674339ad570da01850da
Paasonen Timothy <StrqnDed@protonmail.com>
2019-02-19 22:00:47 +0300

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -6,14 +6,15 @@ import random
 import datetime
 import webbrowser
 import sys
-from currency_converter import CurrencyConverter
 
 jokes = ["Did you hear about the restaurant on the moon? Great food, no atmosphere.", "What do you call a fake noodle? An Impasta.",
 "How many apples grow on a tree? All of them.", "Want to hear a joke about paper? Nevermind it's tearable."]
 
+nets = ['.com', '.net', '.ru', '.io', '.eu']
+
 r = sr.Recognizer()
 
-c = CurrencyConverter()
+root = Tk()
 
 def button_clicked():
     with sr.Microphone() as source:
@@ -22,29 +23,38 @@ def button_clicked():
         text = r.recognize_google(audio)
         print(text)
         if text == 'help':
-            os.system("espeak 'Commands, joke, date, time, help, exit'")
-        if text == 'exit' or 'stop' or 'exit app' or 'stop app':
-            os.system("espeak 'Bye Bye!'")
+            help = 'Commands, joke, date, time, exit'
+            os.system("espeak '{}'".format(help))
+        elif text == 'exit':
+            ex = 'Bye bye!'
+            os.system("espeak '{}'".format(ex))
             sys.exit()
-        if text == 'date' or 'tell the date' or 'tell me the date' or 'what is the date':
+        elif text == 'date':
             date = str(datetime.datetime.now().date())
             os.system("espeak '{}'".format(date))
-        if text == 'time' or 'what is the time' or 'what time is it':
+        elif text == 'time':
             time = str(datetime.datetime.now().time())
             os.system("espeak '{}'".format(time))
-        if text == 'jokes' or 'tell me a joke':
+            labe1 = label(root, textvariable=time)
+        elif nets in text:
+            print('Success')
+        elif text == 'website':
+            url = 'reddit.com'
+            opr = 'Opening Reddit!'
+            os.system("espeak '{}'".format(opr))
+            webbrowser.get('firefox').open(url)
+        elif text == 'jokes' or 'tell me a joke':
             randJoke = random.choice(jokes)
             os.system("espeak '{}'".format(randJoke))
-            
 
-root = Tk()
 
 root.geometry('500x600')
 
 button1 = Button(root, bg='blue', text='Speak!', command=button_clicked, height=2, width=25)
 button1.place(x='140', y='270')
 
-os.system("espeak 'Hey! My name is Andrew! I can help you with basic tasks! Say HELP for commands list'")
+root.mainloop()
 
+#os.system("espeak 'Hey! My name is Andrew! I can help you with basic tasks! Say HELP for commands list'")
 
 root.mainLoop()
